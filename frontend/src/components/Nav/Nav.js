b4cfb6d81b1337c2e75c9f01e035b7526e3b4dfa
Esraa <esraamohamed4037@gmail.com>
2022-12-12 11:27:28 +0200

    add responsive navigation bar

--- frontend/src/components/Nav/Nav.js
+++ frontend/src/components/Nav/Nav.js
@@ -1,23 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Link, NavLink } from "react-router-dom";
 import styles from "./Nav.module.css";
 import Logo from "../Logo/Logo";
+import IonIcon from "@reacticons/ionicons";
 
 function Nav() {
   const [fixNav, setFixNav] = useState(false);
 
+  const navRef = useRef();
+  const toggleMenuBar = () => {
+    navRef.current.classList.toggle(styles["navOpen"]);
+  };
+
   const fixNavbarHandler = () => {
-    if (window.scrollY > 350) {
+    if (window.scrollY > 330) {
       setFixNav(true);
     } else {
       setFixNav(false);
     }
   };
+
   window.addEventListener("scroll", fixNavbarHandler);
+
   return (
-    <header className={`${styles.header} ${fixNav ? styles.fixedNav : null}`}>
+    <header
+      ref={navRef}
+      className={`${styles.header} ${fixNav ? styles.fixedNav : null} `}
+    >
       <Logo />
-      <nav>
+      <nav className={styles["main-nav"]}>
         <ul className={styles["main-nav-list"]}>
           <li>
             <NavLink to="/home" className={styles["main-nav-link"]}>
@@ -39,16 +50,24 @@ function Nav() {
               Add my Company
             </NavLink>
           </li>
+          <li>
+            <Link to="/login" className={styles["nav-cta-login"]}>
+              Log in
+            </Link>
+          </li>
+          <li>
+            <Link to="/signup" className={styles["nav-cta-signup"]}>
+              Sign up
+            </Link>
+          </li>
         </ul>
       </nav>
-      <div className={styles["nav-cta-container"]}>
-        <Link to="/login" className={styles["nav-cta-login"]}>
-          Log in
-        </Link>
-        <Link to="/signup" className={styles["nav-cta-signup"]}>
-          Sign up
-        </Link>
-      </div>
+      <button className={styles.menuBtn} onClick={toggleMenuBar}>
+        <IonIcon name="menu-outline" className={styles.menuIcon}></IonIcon>
+      </button>
+      <button className={styles.closeBtn} onClick={toggleMenuBar}>
+        <IonIcon name="close-outline" className={styles.closeIcon}></IonIcon>
+      </button>
     </header>
   );
 }
